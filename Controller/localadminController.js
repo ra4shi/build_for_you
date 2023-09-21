bdebcb781e080f4c77f1cdeaf420741c6e9a6c44
ra4shi <rashid813895@gmail.com>
2023-09-21 18:09:47 +0530

    updated

--- Controller/localadminController.js
+++ Controller/localadminController.js
@@ -963,7 +963,7 @@ console.log("dashbord data side")
 
   const projectcount =  project.length
 
-  const completedcontruction = await order.find( { contructionstatus : 'success'})  
+
 
     if (!company) {
 
@@ -973,7 +973,7 @@ console.log("dashbord data side")
     const ratingcount = ratings.length
     
 
-    return res.status(200).json({   order, company, ordercount, totalappointmentamount ,completedcontruction  ,projectcount,ratingcount, message: " dashboard data fetched", success: true });
+    return res.status(200).json({   order, company, ordercount, totalappointmentamount  ,projectcount,ratingcount, message: " dashboard data fetched", success: true });
 
 
 
